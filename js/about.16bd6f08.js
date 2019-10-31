cc8ec43cbeb818bbcffb6f8ddc211517794ba9cc
Leitym <53235239+Leitym@users.noreply.github.com>
2019-10-31 20:53:30 +0300

    deploy

---
+++ js/about.16bd6f08.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"out-wrapper"},[a("div",{staticClass:"wrap"},[a("h1",[t._v("This is an about page")])])])}],r=a("2877"),i={},u=Object(r["a"])(i,n,s,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.16bd6f08.js.map
